74912ea3d21b521fb9e766bf9e847c99f3bb8102
Ionică Bizău <bizauionica@gmail.com>
2020-06-02 10:27:27 +0300

    Fix multiple calendars on the page

--- example/js/main.js
+++ example/js/main.js
@@ -1,4 +1,8 @@
-GitHubCalendar(".calendar", "IonicaBizau", {
+GitHubCalendar(".calendar-1", "IonicaBizau", {
+    responsive: true,
+    tooltips: true
+});
+GitHubCalendar(".calendar-2", "bachdx2812", {
     responsive: true,
     tooltips: true
 });
