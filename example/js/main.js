18212ec1a63f1c2d7f61a0fe1645fec145effb37
Ionică Bizău <bizauionica@gmail.com>
2020-11-02 17:39:38 +0200

    Fix #120. Update the dependencies and fix the calendar loading.

--- example/js/main.js
+++ example/js/main.js
@@ -2,7 +2,3 @@ GitHubCalendar(".calendar-1", "IonicaBizau", {
     responsive: true,
     tooltips: true
 });
-GitHubCalendar(".calendar-2", "bachdx2812", {
-    responsive: true,
-    tooltips: true
-});
